2308cd5cf137ce15280e856b3bb8af7319939db7
Patrick Murphy <patmurf@live.com>
2025-07-02 20:26:45 -0500

    basic file read and logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    //get CLI args
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for: {}", query);
+    println!("In filepath: {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Expected to be able to read the file");
+
+    println!("file contents:\n{contents}");
+
 }
