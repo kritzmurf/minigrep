067243c7e101af2ffe0be6d69ab48dd16b7d279d
Patrick Murphy <patmurf@live.com>
2025-07-04 10:00:16 -0500

    refactored using closures

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use std::{env, process};
 use minigrep::{Config, run};
 
 fn main() {
-    //get CLI args
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|error| {
+    let config = Config::build(env::args()).unwrap_or_else(|error| {
         eprintln!("Problem parsing CLI args: {error}");
         process::exit(1);
     });
